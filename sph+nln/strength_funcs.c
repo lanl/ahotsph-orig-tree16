9517ea5e2f625a9680e204f151115a906ffe220a
Carola I. Ellinger <cellinger@lanl.gov>
2016-09-02 14:11:32 -0600

    turn some floats into doubles since k can be huge, plus check bounds
    I'm talking 1E61 huge.

--- sph+nln/strength_funcs.c
+++ sph+nln/strength_funcs.c
@@ -3,28 +3,30 @@
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
+#include <float.h>
 
-void init_defects_table(int gnobj, int Nflaws, float **eps, int ***flaws_tbl_lookup, float kVol, float m) {
+void init_defects_table(int gnobj, int Nflaws, double **eps, int ***flaws_tbl_lookup, float kVol, float m) {
 	int i, j, nflaws_i;
 	int part_id, cur_nflaws, index;
 	int ran;
-	float **eps_act;
+	double **eps_act;
 	float iv_kVol, iv_m;
 
 	/* initialize lookup table */
 	nflaws_i = (int)(Nflaws/gnobj);
 	if (nflaws_i * gnobj < Nflaws)
 		nflaws_i += 1;
+	nflaws_i *= 4;
 	for (i = 0; i < gnobj; i++) {
-		(*flaws_tbl_lookup)[i][1] = i * nflaws_i; /* starting array index in eps[] */
+		(*flaws_tbl_lookup)[i][1] = -1; /* starting array index in eps[] */
 		(*flaws_tbl_lookup)[i][0] = 0; /* number of flaws in particle */
 	}
 
 	/* create a ton of space for all the eps_activation values */
-	eps_act = (float **)malloc ( gnobj * sizeof (float *));
+	eps_act = (double **)malloc ( gnobj * sizeof (double *));
 	for (i = 0; i < gnobj; i++) {
-		eps_act[i] = (float *) malloc (3 * nflaws_i * sizeof (float));
-		for (j = 0; j < 3 * nflaws_i; j++)
+		eps_act[i] = (double *) malloc (nflaws_i * sizeof (double));
+		for (j = 0; j < nflaws_i; j++)
 			eps_act[i][j] = -1.;
 		/*
 		*/
@@ -33,24 +35,33 @@ void init_defects_table(int gnobj, int Nflaws, float **eps, int ***flaws_tbl_loo
 	iv_kVol = 1./kVol;
 	iv_m = 1./m;
 	srand(time(NULL));
-	fprintf(stdout, "RAND_MAX = %d\n", RAND_MAX);
 
 	/* create temporary table of activation thresholds */
 	for (j = 1; j <= Nflaws; j++) {
 		/* select random particle, add flaw */
 		ran = rand();
-		part_id = (int) ((float) ran / (float) (RAND_MAX - 1) * gnobj);
+		/* this is technically part_id-1 */
+		part_id = (int) ((double) ran / (double) (RAND_MAX - 1) * gnobj);
 		cur_nflaws = (*flaws_tbl_lookup)[part_id][0];
+		if (cur_nflaws >= nflaws_i) {
+			fprintf(stdout, "too many flaws in particle\n");
+			exit(1);
+		}
 		(*flaws_tbl_lookup)[part_id][0] += 1;
-		eps_act[part_id][cur_nflaws] = pow ((float) j * iv_kVol, iv_m);
+		eps_act[part_id][cur_nflaws] = pow ((double) j * iv_kVol, iv_m);
 	}
 
 	/* create actual table with flaw activation thresholds */
 	index = 0;
 	for (i = 0; i < gnobj; i++) {
 		cur_nflaws = (*flaws_tbl_lookup)[i][0];
-		(*flaws_tbl_lookup)[i][1] = index;
+		if (cur_nflaws > 0)
+			(*flaws_tbl_lookup)[i][1] = index;
 	//	memcpy (&((*eps)[index]), &(eps_act[i][0]), (size_t) cur_nflaws);
+		if (index + cur_nflaws > Nflaws) {
+			fprintf(stdout, "too many flaws\n");
+			exit(1);
+		}
 		for (j = 0; j < cur_nflaws; j++)
 			(*eps)[index + j] = eps_act[i][j];
 		index += cur_nflaws;
