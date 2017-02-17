6661c31399eb288322a532f62939dc836541bb51
Carola I. Ellinger <cellinger@lanl.gov>
2017-02-17 15:17:04 -0700

    describe parameters in Material_t

--- sph+nln/eos.h
+++ sph+nln/eos.h
@@ -5,10 +5,25 @@ extern double eos_u;
 #ifndef EOS_H
 #define EOS_H
 typedef struct material_s {
-	double rho0;
-	double A, B, a, b, alpha, beta;
-	double Eiv, Ecv;
-	double u0, umelt;
-	double mu, yield, pweib, cweib;
+    double Vol0; /* initial target volume */
+	double rho0; /* original density */
+	double A; /* bulk modulus, units of pressure */
+    double B; /* Tillotson parameter, units of pressure */
+    double a; /* Tillotson parameter */
+    double b; /* Tillotson parameter */
+    double alpha; /* Tillotson parameter */
+    double beta; /* Tillotson parameter */
+	double Eiv; /* energy of incipient vaporization */
+    double Ecv; /* energy of complete vaporization */
+	double u0; /* initial material specific energy density */
+    double umelt; /* melt energy */
+	double mu; /* strain, rho/rho0 - 1 */
+    double yield; /* yield strength */
+    double pweib; /* some Weibull parameter */
+    double cweib; /* some Weibull parameter */
+    double G_shear; /* shear modulus */
+    double E_Young; /* Young's modulus */
+    float material_k; /* material coefficient for flaws */
+    float material_m; /* powerlaw index for flaws */
 } Material_t;
 #endif
